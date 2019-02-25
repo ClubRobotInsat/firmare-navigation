729ad4a4e1b34989b0118fac11c7ba864f08547e
Paul Florence <perso@florencepaul.com>
2019-02-25 22:20:26 +0100

    Clean the code to improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,37 @@
 //! Blink the LED (connected to Pin PC 13) on and off with 1 second interval.
 
-#![allow(unused_imports)]
 #![no_main] //  Don't use the Rust standard bootstrap. We will provide our own.
 #![no_std] //  Don't use the Rust standard library. We are building a binary that can run on its own.
-#[macro_use]
-extern crate cortex_m; //  Low-level functions for ARM Cortex-M3 processor in STM32 Blue Pill.
-#[macro_use(entry, exception)] //  Import macros from the following crates,
-extern crate cortex_m_rt; //  Startup and runtime functions for ARM Cortex-M3.
-extern crate arrayvec;
-extern crate cortex_m_semihosting; //  Debug console functions for ARM Cortex-M3.
-extern crate embedded_hal;
-extern crate librobot;
-extern crate nb;
-extern crate numtoa;
-extern crate panic_semihosting; //  Panic reporting functions, which transmit to the debug console.
-extern crate pid_control;
-extern crate qei;
-extern crate stm32f1xx_hal as bluepill_hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
-use crate::bluepill_hal::stm32 as f103;
 
-use core::fmt::Write;
-use core::fmt::Debug;
+mod robot;
 
-use cortex_m::Peripherals as CortexPeripherals;
-
-use crate::bluepill_hal::prelude::*; //  Define HAL traits.
-use crate::bluepill_hal::qei::Qei;
-use crate::bluepill_hal::serial::Serial;
 use crate::f103::Peripherals;
-use crate::bluepill_hal::time::Hertz;
-use crate::bluepill_hal::spi::*;
-use crate::f103::{SPI1, USART3};
-
-use cortex_m::asm;
-use cortex_m_semihosting::hio; //  For displaying messages on the debug console. //  Clocks, flash memory, GPIO for the STM32 Blue Pill.
-
-use cortex_m_rt::ExceptionFrame; //  Stack frame for exception handling.
-
-use qei::QeiManager;
-
-use w5500::*;
-
-use librobot::navigation::{NavigationFrame, Motor, Pid, RobotConstants, PIDParameters, Command};
-use librobot::transmission::eth::{init_eth, SOCKET_UDP};
+use crate::hal::stm32 as f103;
+use cortex_m::Peripherals as CortexPeripherals;
+use cortex_m_rt::entry;
+use librobot::navigation::{PIDParameters, Pid, RobotConstants};
+use librobot::transmission::eth::init_eth;
 use librobot::units::MilliMeter;
-
-mod PWM;
+#[allow(unused_imports)]
+use panic_semihosting;
+use robot::init_peripherals;
+use stm32f1xx_hal as hal; //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
+use w5500::*;
 
 //  Black Pill starts execution at function main().
 
 #[entry]
 fn main() -> ! {
-    let bluepill = Peripherals::take().unwrap();
-    let _cortex = CortexPeripherals::take().unwrap();
-    let mut debug_out = hio::hstdout().unwrap();
-
-    // Config des horloges
-    let mut rcc = bluepill.RCC.constrain();
-    let mut flash = bluepill.FLASH.constrain();
-    let mut afio = bluepill.AFIO.constrain(&mut rcc.apb2);
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // Config du GPIO
-    let mut gpiob = bluepill.GPIOB.split(&mut rcc.apb2);
-    let mut gpioa = bluepill.GPIOA.split(&mut rcc.apb2);
-    let pa0 = gpioa.pa0; // floating input
-    let pa1 = gpioa.pa1; // floating input
-
-    {
-        let mut pa8 = gpioa.pa8.into_push_pull_output(&mut gpioa.crh);
-        pa8.set_high();
-    }
-
-    let pb0 = gpiob.pb0.into_alternate_push_pull(&mut gpiob.crl);
-    let pb1 = gpiob.pb1.into_alternate_push_pull(&mut gpiob.crl);
-    let pb6 = gpiob.pb6; // floating input
-    let pb7 = gpiob.pb7; // floating input
-    let _pb10 = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
-    let _pb11 = gpiob.pb11.into_floating_input(&mut gpiob.crh);
-    let left_engine_dir_pb8 = gpiob.pb8.into_push_pull_output(&mut gpiob.crh);
-    let right_engine_dir_pb9 = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
-
-    // Config des QEI
-    let qei_right =
-        QeiManager::new(Qei::tim2(bluepill.TIM2, (pa0, pa1), &mut afio.mapr, &mut rcc.apb1));
-    let qei_left =
-        QeiManager::new(Qei::tim4(bluepill.TIM4, (pb6, pb7), &mut afio.mapr, &mut rcc.apb1));
-
-    // Config des PWM
-    let (mut pwm_right_pb0, mut pwm_left_pb1) = bluepill
-        .TIM3
-        .pwm((pb0, pb1),
-             &mut afio.mapr,
-             Hertz(10000),
-             clocks,
-             &mut rcc.apb1);
-    pwm_right_pb0.enable();
-    pwm_left_pb1.enable();
-    let max_duty = pwm_right_pb0.get_max_duty();
-
-    let mut motor_left = Motor::new(pwm_left_pb1, left_engine_dir_pb8);
-    let mut motor_right = Motor::new(pwm_right_pb0, right_engine_dir_pb9);
+    let chip = Peripherals::take().unwrap();
+    let cortex = CortexPeripherals::take().unwrap();
+    let mut robot = init_peripherals(chip, cortex);
+    let mut eth = W5500::new(&mut robot.spi_eth, &mut robot.cs);
+    init_eth(
+        &mut eth,
+        &mut robot.spi_eth,
+        &MacAddress::new(0x02, 0x01, 0x02, 0x03, 0x04, 0x05),
+        &IpAddress::new(255, 255, 255, 0),
+    );
 
     // Config du PID
     let pid_parameters = PIDParameters {
@@ -110,14 +41,19 @@ fn main() -> ! {
         pos_kd: 1.0,
         orient_kp: 1.0,
         orient_kd: 1.0,
-        max_output: max_duty / 4,
+        max_output: robot.max_duty / 4,
     };
     let robot_constants = RobotConstants {
         coder_radius: MilliMeter(31),
         inter_axial_length: MilliMeter(223),
     };
 
-    let mut pos_pid = Pid::new(qei_left, qei_right, &pid_parameters, robot_constants);
+    let mut pos_pid = Pid::new(
+        robot.qei_left,
+        robot.qei_right,
+        &pid_parameters,
+        robot_constants,
+    );
 
     // ==== Config de l'ethernet
     // ports pour la com
@@ -150,7 +86,7 @@ fn main() -> ! {
 
     //pos_pid.forward(MilliMeter(50));
 
-    let mut buffer = [0; 2048];
+    let _buffer = [0; 2048];
 
     loop {
         /*if let Some((_, _, size)) =
@@ -170,28 +106,12 @@ fn main() -> ! {
         let (cmd_left, cmd_right) = pos_pid.get_command();
 
         // permet d'afficher les valeurs des qei pour le debug
-        write!(debug_out, "{:?}\n", pos_pid).unwrap();
+        //write!(debug_out, "{:?}\n", pos_pid).unwrap();
 
         // Permet d'afficher les valeurs des commandes moteur pour le debug
-        write!(debug_out, "Left : {}, Right : {}\n", cmd_left, cmd_right).unwrap();
+        //write!(debug_out, "Left : {}, Right : {}\n", cmd_left, cmd_right).unwrap();
 
-        motor_left.apply_command(cmd_left);
-        motor_right.apply_command(cmd_right);
+        robot.motor_left.apply_command(cmd_left);
+        robot.motor_right.apply_command(cmd_right);
     }
-}
-
-//  For any hard faults, show a message on the debug console and stop.
-
-#[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    asm::bkpt();
-    panic!("Hard fault: {:#?}", ef);
-}
-
-//  For any unhandled interrupts, show a message on the debug console and stop.
-
-#[exception]
-fn DefaultHandler(irqn: i16) {
-    asm::bkpt();
-    panic!("Unhandled exception (IRQn = {})", irqn);
 }
---
+++ src/robot.rs
@@ -0,0 +1,190 @@
+use crate::f103;
+use crate::f103::{interrupt, Peripherals, SPI1, TIM2, TIM3, TIM4};
+use crate::hal::delay::Delay;
+use crate::hal::gpio::{
+    gpioa::*, gpiob::*, gpioc::*, Alternate, Floating, Input, Output, PushPull,
+};
+use crate::hal::prelude::*;
+use crate::hal::pwm::{Pwm, C3, C4};
+use crate::hal::qei::Qei;
+use crate::hal::spi::*;
+use crate::hal::timer::Timer;
+use crate::CortexPeripherals;
+use cortex_m_rt::exception;
+use cortex_m_rt::ExceptionFrame;
+use librobot::navigation::Motor;
+use qei::QeiManager; //  Stack frame for exception handling.
+
+type QeiLeft =
+    (qei::QeiManager<stm32f1xx_hal::qei::Qei<TIM4, (PB6<Input<Floating>>, PB7<Input<Floating>>)>>);
+
+type QeiRight =
+    (qei::QeiManager<stm32f1xx_hal::qei::Qei<TIM2, (PA0<Input<Floating>>, PA1<Input<Floating>>)>>);
+
+type MotorLeft = Motor<Pwm<TIM3, C4>, PB8<Output<PushPull>>>;
+type MotorRight = Motor<Pwm<TIM3, C3>, PB9<Output<PushPull>>>;
+
+type SpiPins = (
+    PA5<Alternate<PushPull>>,
+    PA6<Input<Floating>>,
+    PA7<Alternate<PushPull>>,
+);
+
+pub struct Robot<K, P> {
+    pub spi_eth: Spi<K, P>,
+    pub delay: Delay,
+    pub led_black_pill: PC13<Output<PushPull>>,
+    pub qei_left: QeiLeft,
+    pub qei_right: QeiRight,
+    pub cs: PB13<Output<PushPull>>,
+    pub motor_right: MotorRight,
+    pub motor_left: MotorLeft,
+    pub max_duty: u16,
+}
+
+pub fn init_peripherals(chip: Peripherals, mut cortex: CortexPeripherals) -> Robot<SPI1, SpiPins> {
+    //  Get the clocks from the STM32 Reset and Clock Control (RCC) and freeze the Flash Access Control Register (ACR).
+    // Config des horloges
+    let mut rcc = chip.RCC.constrain();
+    let mut flash = chip.FLASH.constrain();
+    let mut afio = chip.AFIO.constrain(&mut rcc.apb2);
+
+    cortex.DCB.enable_trace();
+    cortex.DWT.enable_cycle_counter();
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(72.mhz())
+        .pclk1(36.mhz())
+        .pclk2(72.mhz())
+        .freeze(&mut flash.acr);
+
+    //  Configuration des GPIOs
+    let mut gpioa = chip.GPIOA.split(&mut rcc.apb2);
+    let mut gpiob = chip.GPIOB.split(&mut rcc.apb2);
+    let mut gpioc = chip.GPIOC.split(&mut rcc.apb2);
+
+    // Configuration des PINS
+
+    // Slave select, on le fixe à un état bas (on n'en a pas besoin, une seule communication)
+    let mut cs = gpiob.pb13.into_push_pull_output(&mut gpiob.crh);
+    cs.set_low();
+
+    let sclk = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6.into_floating_input(&mut gpioa.crl);
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    {
+        // Hardfault LED
+        let mut pin = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+        pin.set_low();
+        // Blinking led
+        let mut led = gpioc.pc14.into_push_pull_output(&mut gpioc.crh);
+        led.set_low();
+    }
+    let led_black_pill = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    let spi = Spi::spi1(
+        chip.SPI1,
+        (sclk, miso, mosi),
+        &mut afio.mapr,
+        Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnFirstTransition,
+        },
+        1.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    // Clignotement de la led
+    let _ = Timer::tim1(chip.TIM1, 5.hz(), clocks, &mut rcc.apb2);
+
+    // Config des QEI
+    let pb0 = gpiob.pb0.into_alternate_push_pull(&mut gpiob.crl);
+    let pb1 = gpiob.pb1.into_alternate_push_pull(&mut gpiob.crl);
+    let pb6 = gpiob.pb6; // floating input
+    let pb7 = gpiob.pb7; // floating input
+    let pa0 = gpioa.pa0; // floating input
+    let pa1 = gpioa.pa1; // floating input
+    let left_engine_dir_pb8 = gpiob.pb8.into_push_pull_output(&mut gpiob.crh);
+    let right_engine_dir_pb9 = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
+
+    let qei_right = QeiManager::new(Qei::tim2(
+        chip.TIM2,
+        (pa0, pa1),
+        &mut afio.mapr,
+        &mut rcc.apb1,
+    ));
+    let qei_left = QeiManager::new(Qei::tim4(
+        chip.TIM4,
+        (pb6, pb7),
+        &mut afio.mapr,
+        &mut rcc.apb1,
+    ));
+
+    // Config des PWM
+    let (mut pwm_right_pb0, mut pwm_left_pb1) = chip.TIM3.pwm(
+        (pb0, pb1),
+        &mut afio.mapr,
+        10000.hz(),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    pwm_right_pb0.enable();
+    pwm_left_pb1.enable();
+
+    let max_duty = pwm_right_pb0.get_max_duty();
+
+    let motor_left = Motor::new(pwm_left_pb1, left_engine_dir_pb8);
+    let motor_right = Motor::new(pwm_right_pb0, right_engine_dir_pb9);
+
+    //  Create a delay timer from the RCC clocks.
+    let delay = Delay::new(cortex.SYST, clocks);
+
+    Robot {
+        spi_eth: spi,
+        delay,
+        led_black_pill,
+        cs,
+        motor_right,
+        qei_left,
+        qei_right,
+        motor_left,
+        max_duty,
+    }
+}
+
+#[interrupt]
+fn TIM1_UP() {
+    static mut TOOGLE: bool = false;
+    unsafe {
+        if *TOOGLE {
+            (*f103::GPIOC::ptr()).bsrr.write(|w| w.br14().set_bit());
+        } else {
+            (*f103::GPIOC::ptr()).bsrr.write(|w| w.bs14().set_bit());
+        }
+        *TOOGLE = !(*TOOGLE);
+        (*f103::TIM3::ptr()).sr.write(|w| w.uif().clear_bit());
+    }
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    unsafe {
+        (*f103::GPIOB::ptr()).bsrr.write(|w| w.br12().set_bit());
+    }
+    panic!("Hard fault: {:#?}", ef);
+}
+
+//  For any unhandled interrupts, show a message on the debug console and stop.
+
+#[exception]
+fn DefaultHandler(irqn: i16) {
+    unsafe {
+        (*f103::GPIOB::ptr()).bsrr.write(|w| w.br7().set_bit());
+    }
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
